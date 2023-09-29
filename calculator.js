ed44e1afa627a42d418c848c2fe71a9ba96e38f3
Nishanth <nishanth@pop-os.localdomain>
2023-09-29 12:30:21 +0530

    Update-4

--- calculator.js
+++ calculator.js
@@ -1,9 +1,14 @@
 let calculation = '';
       
-      function cap() {
-        document.querySelector('.displayNumbers').innerHTML = calculation;
-      }
+function cap() {
+  document.querySelector('.displayNumbers').innerHTML = calculation;
+}
 
-      function result() {
-        document.querySelector('.displayNumbers').innerHTML = eval(calculation);
-      }
+function result() {
+  document.querySelector('.displayNumbers').innerHTML = eval(calculation);
+}
+
+function del() {
+  calculation = calculation.slice(0,-1);
+  document.querySelector('.displayNumbers').innerHTML = calculation;
+}
